706922270f198d58a444c74b96d5a30510763439
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-21 14:03:47 -0500

    added multiple validation checks for username, password and email

--- src/main/webapp/WEB-INF/js/validate.js
+++ src/main/webapp/WEB-INF/js/validate.js
@@ -1,25 +1,116 @@
 (function () {
 	"use strict";
+	const baseUrl = "http://localhost:8080";
+	const validateEndpoint = "/validate";
+	const minLength = 3;
+	const maxLength = 256;
 
-	function validatePassword() {
-		const password = $("#password").val();
-		const confirmPassword = $("#confirm_password").val();
-		return password === confirmPassword && confirmPassword.trim().length > 0;
+	async function usernameExistsAlready(element) {
+		const query = `?username=${encodeURIComponent(element.val())}`;
+		return await fetch(baseUrl + validateEndpoint + query)
+			.then(response => response.json())
+			.then(result => result);
 	}
 
-	function formatConfirmPassword() {
-		if (!validatePassword()) {
-			$("#confirm_password")
-				.addClass("is-invalid")
-				.removeClass("is-valid")
-				.get()[0].setCustomValidity("Password must match.");
+	function containsSpaces(element) {
+		return element.val().includes(" ");
+	}
+
+	function lengthIsInBounds(element) {
+		return element.val().length >= minLength && element.val().length <= maxLength;
+	}
+
+	function passwordsMatch(password, confirmPassword) {
+		if (confirmPassword.length > 0) {
+			return password.val() === confirmPassword.val() && confirmPassword.val().trim().length > 0;
 		} else {
-			$("#confirm_password")
-				.removeClass("is-invalid")
-				.addClass("is-valid")
-				.get()[0].setCustomValidity("");
+			return true;
 		}
 	}
+
+	function passwordIsSecure(password) {
+		//Regex taken from: https://stackoverflow.com/questions/32311081/check-for-special-characters-in-string
+		const special = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+		const numbers = /[0-9]/;
+		const uppercase = /[A-Z]/;
+		return special.test(password.val()) && numbers.test(password.val()) && uppercase.test(password.val());
+	}
+
+	function formatValid(element) {
+		element.addClass("is-valid").removeClass("is-invalid").removeClass("is-loading");
+	}
+
+	function formatInvalid(element) {
+		element.addClass("is-invalid").removeClass("is-valid").removeClass("is-loading");
+	}
+
+	function formatLoading(element) {
+		element.addClass("is-loading").removeClass("is-invalid").removeClass("is-valid");
+	}
+
+	function setAndDisplayValidityMessage(element, message) {
+		element.get()[0].setCustomValidity(message);
+		element.siblings(".invalid-feedback").first().text(message);
+	}
+
+	$("#username").on("keyup",async function () {
+		formatInvalid($(this));
+		if (!lengthIsInBounds($(this))) {
+			setAndDisplayValidityMessage($(this),"Username is too short or too long");
+			return;
+		}
+		if (containsSpaces($(this))) {
+			setAndDisplayValidityMessage($(this),"Username must not contain spaces");
+			return;
+		}
+		formatLoading($(this));
+		const user = await usernameExistsAlready($(this));
+		if (user !== null) {
+			formatInvalid($(this));
+			setAndDisplayValidityMessage($(this), "Username " + user.username + " is already taken.");
+			return;
+		}
+		formatValid($(this));
+		setAndDisplayValidityMessage($(this), "");
+	});
+	$("#email").on("keyup", function () {
+		formatInvalid($(this));
+		if (!lengthIsInBounds($(this))) {
+			setAndDisplayValidityMessage($(this),"Email is too short or too long");
+		} else if (containsSpaces($(this))) {
+			setAndDisplayValidityMessage($(this),"Email must not contain spaces");
+		} else {
+			formatValid($(this));
+			setAndDisplayValidityMessage($(this), "");
+		}
+	})
+
+	$("#password").on("keyup", function () {
+		formatInvalid($(this));
+		if (!lengthIsInBounds($(this))) {
+			setAndDisplayValidityMessage($(this),"Password is too short or too long");
+		} else if (containsSpaces($(this))) {
+			setAndDisplayValidityMessage($(this),"Password must not contain spaces");
+		} else if (!passwordIsSecure($(this))) {
+			setAndDisplayValidityMessage($(this),"Password must contain at least one special character, one number, and one uppercase letter.");
+		} else {
+			formatValid($(this));
+			setAndDisplayValidityMessage($(this), "");
+		}
+	});
+
+	$(".passwords-match-validation").each(function () {
+		$(this).on("keyup", function () {
+			let confirmPassword = $("#confirm_password");
+			if (!passwordsMatch($("#password"), confirmPassword)) {
+				formatInvalid(confirmPassword);
+				setAndDisplayValidityMessage(confirmPassword, "Passwords do not match");
+			} else {
+				formatValid(confirmPassword);
+				setAndDisplayValidityMessage(confirmPassword, "");
+			}
+		});
+	});
 
 	$(".needs-validation").each(function (idx) {
 		$(this).on("submit", function (e) {
@@ -28,23 +119,7 @@
 				e.preventDefault();
 				e.stopPropagation();
 			}
-			if (!validatePassword()) {
-				e.preventDefault();
-				e.stopPropagation();
-			} else {
-				$("#confirm_password").get()[0].setCustomValidity("");
-			}
 			$(this).addClass("was-validated");
 		});
-	});
-
-	$(".passwords-match-validation").each(function () {
-		$(this).on("keyup", function () {
-			formatConfirmPassword();
-		}).on("submit", function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-			formatConfirmPassword();
-		})
 	});
 })();
