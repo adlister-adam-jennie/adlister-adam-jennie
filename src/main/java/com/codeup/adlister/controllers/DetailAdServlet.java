5d4dbd14198c6de701fb44cfe1b8907e57f87da7
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-21 14:07:25 -0500

    update"

--- src/main/java/com/codeup/adlister/controllers/DetailAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DetailAdServlet.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.dao.MySQLAdsDao;
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Category;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -11,11 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "controllers.DetailAdServlet", urlPatterns = "/ads/detail")
 public class DetailAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         long adId = Integer.parseInt(request.getParameter("id"));
+        List<Category> categories = request.getParameter("categories");
         Ad ad = DaoFactory.getAdsDao().getAd(adId);
         request.getSession().setAttribute("ad", ad);
 
@@ -23,6 +26,8 @@ public class DetailAdServlet extends HttpServlet {
         long userId = Integer.parseInt(request.getParameter("userId"));
         User user = DaoFactory.getUsersDao().findByUserId(userId);
         request.getSession().setAttribute("user", user);
+
+
 
 //        need to grab the categories as well
 
