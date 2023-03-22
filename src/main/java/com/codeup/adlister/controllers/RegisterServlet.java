1bbdeaf8f4b9b36061b687322f914b08e4118fb1
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-22 10:46:01 -0500

    Users that are already logged in are redirected to the profile page

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -14,7 +14,11 @@ import java.io.IOException;
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        if (request.getSession().getAttribute("user") == null) {
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/profile");
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
