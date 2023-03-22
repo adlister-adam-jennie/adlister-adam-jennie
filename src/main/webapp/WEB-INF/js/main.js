562c3dbeed9757799437e28e2b61cb9cd0823812
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-22 12:13:37 -0500

    update

--- src/main/webapp/WEB-INF/js/main.js
+++ src/main/webapp/WEB-INF/js/main.js
@@ -6,11 +6,22 @@ const ads = (function () {
 		array.push({
 			id: $(this).attr("data-ad-id"),
 			title: $(this).children(".ad-title").first().text(),
-			description: $(this).children(".ad-description").first().text()
+			description: $(this).children(".ad-description").first().text(),
+			categories: getCategories($(this).find("#ad-categories").children(".ad-category"))
 		});
 	})
 	return array;
 })();
+
+function getCategories(categories){
+	const array = [];
+	categories.each(function (){
+		array.push(
+			$(this).val()
+		)
+	})
+	return array;
+}
 
 function hideAllAdDivs() {
 	$(".ad").each(function () {
@@ -24,22 +35,36 @@ function showAllAdDivs() {
 	})
 }
 
-function searchAds(input) {
-	if (input === "") {
+function searchAds(searchTerm, catValue) {
+	if (searchTerm === "" && catValue[0] === "0") {
 		showAllAdDivs();
 		return;
 	}
 	hideAllAdDivs();
 	for (let ad of ads) {
-		if (ad.title.toLowerCase().includes(input)) {
+		console.log(ad.categories)
+		if (ad.title.toLowerCase().includes(searchTerm)) {
 			$(`[data-ad-id='${ad.id}']`).first().removeClass("d-none");
 		}
-		if (ad.description.toLowerCase().includes(input)) {
+		if (ad.description.toLowerCase().includes(searchTerm)) {
 			$(`[data-ad-id='${ad.id}']`).first().removeClass("d-none");
+		}
+		for (let category of ad.categories){
+			for (let value of catValue){
+				if (value == category){
+					$(`[data-ad-id='${ad.id}']`).first().removeClass("d-none");
+				}
+			}
 		}
 	}
 }
 
+$("#ad-category").on("change",function (){
+	searchAds($("#ad-search").val().toLowerCase().trim(), $(this).val());
+
+})
+
+
 $("#ad-search").on("keyup", function (){
-	searchAds($(this).val().toLowerCase().trim());
+	searchAds($(this).val().toLowerCase().trim(), $(this).val());
 });
